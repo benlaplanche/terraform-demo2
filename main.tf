492fc188f3564af8823bfde0e856fcecfdc3f7a1
Ben Laplanche <ben@laplanche.co.uk>
2020-07-30 15:55:29 +0100

    insecure file

--- main.tf
+++ main.tf
@@ -9,22 +9,11 @@ provider "aws" {
 }
 
 resource "aws_iam_account_password_policy" "strict" {
-  minimum_password_length        = 14
+  minimum_password_length        = 8
   allow_users_to_change_password = false
   password_reuse_prevention      = 24
-  require_numbers                = true
-  require_symbols                = true
-  require_lowercase_characters   = true
-  require_uppercase_characters   = true
 }
 
 resource "aws_s3_bucket" "insecure-bucket" {
   bucket = "insecure-bucket"
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
 }
