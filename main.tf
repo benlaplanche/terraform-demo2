5f836d3b8ac0d34d73fdce79baa462e9a8a50d2b
Ben Laplanche <ben@laplanche.co.uk>
2020-07-30 13:25:16 +0100

    initial insecure file

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  version                     = "~> 2.67"
+  region                      = "eu-west-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
+
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 8
+  allow_users_to_change_password = false
+  password_reuse_prevention      = 24
+  max_password_age               = 3
+}
+
+resource "aws_s3_bucket" "insecure-bucket" {
+  bucket = "insecure-bucket"
+}
